14a4e4b7e709eb637faf2afea5d4e2c8a4dcf77b
unknown <favobuchi@outlook.com>
2022-08-18 15:30:44 +0100

    Included javascript files

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    //grab content from feet input
+let feet = document.querySelector('#feet');
+let inches = document.querySelector('#inches');
+const results = document.querySelector('#results');
+    e.preventDefault();
+ 
+})
